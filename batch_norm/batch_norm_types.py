5ec54da32f50e1592eca8776fc94e69ef02b544f
bettersituation <xogml9365@gmail.com>
2019-05-10 22:44:02 +0900

    tmp save

--- batch_norm/batch_norm_types.py
+++ batch_norm/batch_norm_types.py
@@ -2,17 +2,19 @@ from functools import partial
 import tensorflow as tf
 
 
-def none_batch_norm(inputs):
+def none_batch_norm(inputs, *args):
+    return inputs, 0.
+
+
+def batch_norm(inputs, training_ph, *args):
+    with tf.variable_scope('bn'):
+        gamma = tf.get_variable('gamma', shape=inputs.get_shape())
     pass
 
 
-def batch_norm(inputs):
+def rigid_batch_norm(inputs, training_ph, bound, *args):
     pass
 
 
-def rigid_batch_norm(inputs):
-    pass
-
-
-def clipped_rigid_batch_norm(inputs, clip_value):
+def clipped_rigid_batch_norm(inputs, training_ph, bound, clip_value):
     pass
--- network/vgg_cifar.py
+++ network/vgg_cifar.py
@@ -24,13 +24,13 @@ class VGG:
 
     def train(x, y):
         feed_dict = {self.inputs_ph: x, self.labels_ph: y, self.training_ph: True}
-        targets = [self.values_dict['loss'], self.train_op]
-        loss, _ = self.sess.run(targets, feed_dict = feed_dict)
-        return loss
+        targets = [self.values_dict['loss'], self.values_dict['acc'], self.train_op]
+        loss, acc, _ = self.sess.run(targets, feed_dict = feed_dict)
+        return loss, acc
 
     def test(x):
         feed_dict = {self.inputs_ph: x, self.training_ph: False}
-        loss, predicts = self.sess.run(self.values_dict['predicts'], feed_dict=feed_dict)
+        predicts = self.sess.run(self.values_dict['predicts'], feed_dict=feed_dict)
         return predicts
 
     def get_bn_values(x):
@@ -44,15 +44,17 @@ class VGG:
         return gradients
 
     def _set_graph():
+        vgg16_conv_layers = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+        vgg19_conv_layers = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
         if vgg_name == 'vgg16':
-            self._set_vgg16()
+            self._set_vgg(vgg16_conv_layers)
         elif vgg_name == 'vgg19':
-            self._set_vgg19()
+            self._set_vgg(vgg19_conv_layers)
 
         opt = tf.train.GradientDescentOptimizer(self.lr)
         self.train_op = opt.minimize(self.values_dict['loss_with_reg'])
 
-    def _set_vgg16():
+    def _set_vgg(conv_layers):
         self.values_dict['reg_loss'] = 0.
         dense = partial(tf.layers.dense, activation=tf.nn.relu)
         last_dense = partial(tf.layers.dense, activation=tf.nn.softmax)
@@ -61,7 +63,7 @@ class VGG:
         bn_func = self._get_bn_func()
         store_bn = self.store_bn_values
         inputs = self.inputs_ph
-        for i, layers in enumerate([64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']):
+        for i, layers in enumerate(conv_layers):
             if isinstance(layers, int):
                 inputs, reg_loss = bn_func(conv(inputs, layers))
                 self.values_dict['reg_loss'] += reg_loss
@@ -79,41 +81,10 @@ class VGG:
         self.values_dict['predicts'] = predicts
 
         loss = - tf.reduce_sum(self.labels_ph * tf.log(predicts + 1e-8) + (1 - self.labels_ph) * tf.log(1 - predicts + 1e-8))
+
         self.values_dict['loss'] = loss
-
         self.values_dict['loss_with_reg'] = loss + self.reg_cf * self.values_dict['reg_loss']
-
-    def _set_vgg19():
-        self.values_dict['reg_loss'] = 0.
-        dense = partial(tf.layers.dense, activation=tf.nn.relu)
-        last_dense = partial(tf.layers.dense, activation=tf.nn.softmax)
-        conv = partial(tf.layers.conv2d, kernel_size=(3, 3), strides=1, padding='same', activation=tf.nn.relu)
-        max_pool = partial(tf.layers.max_pooling2d, pool_size=(2, 2), strides=2)
-        bn_func = self._get_bn_func()
-        store_bn = self.store_bn_values
-        inputs = self.inputs_ph
-        for layers in enumerate([64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M',
-                                 512, 512, 512, 512, 'M']):
-            if isinstance(layers, int):
-                inputs, reg_loss = bn_func(conv(inputs, layers))
-                self.values_dict['reg_loss'] += reg_loss
-                store_bn('conv_{}'.format(i), inputs)
-            else:
-                inputs = max_pool(inputs)
-
-        inputs = inputs.flatten()
-        for i, layers in enumerate([512, 512]):
-            inputs, reg_loss = bn_func(dense(inputs, layers))
-            self.values_dict['reg_loss'] += reg_loss
-            store_bn('dense_{}'.format(i), inputs)
-
-        predicts = last_dense(inputs, self.labels_num)
-        self.values_dict['predicts'] = predicts
-
-        loss = - tf.reduce_sum(self.labels_ph * tf.log(predicts + 1e-8) + (1 - self.labels_ph) * tf.log(1 - predicts + 1e-8))
-        self.values_dict['loss'] = loss
-
-        self.values_dict['loss_with_reg'] = loss + self.reg_cf * self.values_dict['reg_loss']
+        self.values_dict['acc'] = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predicts, 1), tf.argmax(tf.labels_ph, 1)), tf.float32))
 
     def _set_gradients():
         vs = tf.trainable_variables():
