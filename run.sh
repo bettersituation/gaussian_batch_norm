40d8b257b30c9d17c9666a2137a63828fb6e35fd
taehee.cho <taehee.cho@qraftec.com>
2019-05-13 02:13:16 +0900

    lr comparison setting

--- run.sh
+++ run.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
-python run.py --data_type mnist --net_name simple --batch_norm none --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-python run.py --data_type mnist --net_name simple --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-python run.py --data_type mnist --net_name simple --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
+# simple comparison mnist
+python run.py --data_type mnist --net_name simple --batch_norm none --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 50
+python run.py --data_type mnist --net_name simple --batch_norm batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 50
+python run.py --data_type mnist --net_name simple --batch_norm rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 50
+python run.py --data_type mnist --net_name simple --batch_norm clipped_rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 50
 
-python run.py --data_type cifar10 --net_name simple --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-python run.py --data_type cifar10 --net_name simple --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
+# simple comparison cifar10
+python run.py --data_type cifar10 --net_name simple --batch_norm batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name simple --batch_norm rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name simple --batch_norm clipped_rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 200
 
-python run.py --data_type cifar10 --net_name vgg16 --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-python run.py --data_type cifar10 --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
+# vgg16 default comparison
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm clipped_rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.001 --batch_size 100 --epoch 200
 
-python run.py --data_type cifar10 --net_name vgg16 --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.01 --batch_size 100
-python run.py --data_type cifar10 --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.01 --batch_size 100
-python run.py --data_type cifar10 --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.005 --batch_size 100
+# lr 0.01 comparison
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm batch_norm --bound 5 --reg_cf 1 --lr 0.01 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.01 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm clipped_rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.01 --batch_size 100 --epoch 200
 
-python run.py --data_type cifar100 --net_name vgg16 --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-python run.py --data_type cifar100 --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-
-python run.py --data_type cifar100 --net_name vgg16 --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.01 --batch_size 100
-python run.py --data_type cifar100 --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.01 --batch_size 100
-python run.py --data_type cifar100 --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.005 --batch_size 100
-
-python run.py --data_type fashion_mnist --net_name vgg16 --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-python run.py --data_type fashion_mnist --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.001 --batch_size 100
-
-python run.py --data_type fashion_mnist --net_name vgg16 --batch_norm batch_norm --bound 10 --reg_cf 1 --lr 0.01 --batch_size 100
-python run.py --data_type fashion_mnist --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.01 --batch_size 100
-python run.py --data_type fashion_mnist --net_name vgg16 --batch_norm rigid_batch_norm --bound 10 --reg_cf 1 --lr 0.005 --batch_size 100
+# lr 0.005 comparison
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm batch_norm --bound 5 --reg_cf 1 --lr 0.005 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.005 --batch_size 100 --epoch 200
+python run.py --data_type cifar10 --net_name vgg16 --batch_norm clipped_rigid_batch_norm --bound 5 --reg_cf 1 --lr 0.005 --batch_size 100 --epoch 200
